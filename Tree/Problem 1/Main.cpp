0b31aa7a098668a3b587ad607b1e2e79f25669a8
unknown <Administrator@umkanojadell.fareast.corp.microsoft.com>
2019-02-09 01:37:31 +0530

    taking file through cmd

--- Tree/Problem 1/Main.cpp
+++ Tree/Problem 1/Main.cpp
@@ -4,61 +4,73 @@ You have given an array and you have to replace values of array with t^th parent
 Root nodes parent will have -1 value and if t^th parent doesn't exist, then add -1.
 */
 
-#include<iostream>
-#include<fstream>
-#include<vector>
+#include <iostream>
+#include <fstream>
+#include <vector>
 
 using namespace std;
 
-void replaceParentWithTthParent(int **arr, int length, int tParent) {
+void replaceParentWithTthParent(int **arr, int length, int tParent)
+{
 	vector<int> storeTthParent(length, -2);
 	int *finalArray = new int[length], *array = *arr;
-	for (int i=0; i<length; i++) {
-		if (array[i] == -1) {
+	for (int i = 0; i < length; i++)
+	{
+		if (array[i] == -1)
+		{
 			finalArray[i] = array[i];
 			continue;
 		}
-		
-		if (storeTthParent[array[i]] != -2) finalArray[i] = storeTthParent[array[i]];
+
+		if (storeTthParent[array[i]] != -2)
+			finalArray[i] = storeTthParent[array[i]];
 		int parentIndex = array[i], oldParent = array[i];
-		for (int j=tParent; j > 1; j--) {
-			if (parentIndex == -1) break;
+		for (int j = tParent; j > 1; j--)
+		{
+			if (parentIndex == -1)
+				break;
 			parentIndex = array[parentIndex];
 		}
-		
+
 		finalArray[i] = parentIndex;
 		storeTthParent[oldParent] = parentIndex;
 	}
-	
+
 	*arr = finalArray;
-	
-	cout<<"\nParent array is: \n";
-	for (int i=0; i<length; i++){
-		cout<<i<<"->"<<storeTthParent[i]<<"\n";
+
+	cout << "\nParent array is: \n";
+	for (int i = 0; i < length; i++)
+	{
+		cout << i << "->" << storeTthParent[i] << "\n";
 	}
 }
 
-int main() {
-	ifstream infile("input.txt");
+int main()
+{
+	//ifstream infile("input.txt");
 	int n, tParent;
-	infile>>n;
+	cin >> n;
 	int *tree = new int[n];
-	for (int i=0; i<n; i++) {
-		infile>>tree[i];
+	for (int i = 0; i < n; i++)
+	{
+		cin >> tree[i];
 	}
-	
-	infile.close(); 
-	cout<<"Given array is: \n";
-	for (int i=0; i<n; i++){
-		cout<<i<<"->"<<tree[i]<<" \n";
+
+	//infile.close();
+	cout << "Given array is: \n";
+	for (int i = 0; i < n; i++)
+	{
+		cout << i << "->" << tree[i] << " \n";
 	}
-	
-	cout<<"\n\nReplace parent value with t^th parent = ";
-	cin>>tParent;
-	
+
+	cout << "\n\nReplace parent value with t^th parent = ";
+	cin >> tParent;
+	cout << tParent << endl;
+
 	replaceParentWithTthParent(&tree, n, tParent);
-	cout<<"\nNew array is: \n";
-	for (int i=0; i<n; i++){
-		cout<<i<<"->"<<tree[i]<<" \n";
+	cout << "\nNew array is: \n";
+	for (int i = 0; i < n; i++)
+	{
+		cout << i << "->" << tree[i] << " \n";
 	}
 }
